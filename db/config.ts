3a01cf2321480c23a1961f72a4c1ee68b5c4474c
Thomas Jensen <thomas.jensen_@outlook.com>
2025-02-03 20:12:29 +0100

    my failsafes aren't playing nice with meh

--- db/config.ts
+++ db/config.ts
@@ -2,8 +2,4 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 
 const connString = Deno.env.get("SUPABASE_URL")!
 
-if (!connString) {
-  throw new Error("Missing Supabase credentials. Please set SUPABASE_URL.");
-}
-
 export const db = drizzle(connString);
