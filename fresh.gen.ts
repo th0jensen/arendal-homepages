c6e6b612695ef04aa3a10602961591dbc1b7f95c
Thomas Jensen <thomas.jensen_@outlook.com>
2025-02-05 08:11:07 +0100

    Now we should be bueno

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,10 +5,8 @@
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $api_companies from "./routes/api/companies.ts";
-import * as $api_trpc_trpc_ from "./routes/api/trpc/[trpc].ts";
 import * as $index from "./routes/index.tsx";
 import * as $CompanyTable from "./islands/CompanyTable.tsx";
-import * as $Counter from "./islands/Counter.tsx";
 import type { Manifest } from "$fresh/server.ts";
 
 const manifest = {
@@ -16,12 +14,10 @@ const manifest = {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
     "./routes/api/companies.ts": $api_companies,
-    "./routes/api/trpc/[trpc].ts": $api_trpc_trpc_,
     "./routes/index.tsx": $index,
   },
   islands: {
     "./islands/CompanyTable.tsx": $CompanyTable,
-    "./islands/Counter.tsx": $Counter,
   },
   baseUrl: import.meta.url,
 } satisfies Manifest;
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,10 +1,23 @@
-import { type PageProps } from "$fresh/server.ts";
+import type { PageProps, Handlers } from "$fresh/server.ts";
+import { CompanySelect } from "../db/schema.ts";
 import CompanyTable from "../islands/CompanyTable.tsx";
-import { trpcClient } from "../utils/trpc-client.ts";
+import { handler as companiesHandler } from "./api/companies.ts";
 
+export const handler: Handlers = {
+  async GET(_req, ctx) {
+    const headers = new Headers({
+      'Accept-Encoding': 'gzip',
+      'Content-Type': 'application/json'
+    });
+    const req = new Request(_req, { headers });
+    const resp = await companiesHandler(req, ctx);
+    const companies: CompanySelect[] = await resp.json();
+    return ctx.render({ companies });
+  },
+};
 
-export default async function Home(props: PageProps) {
-  const companies = await trpcClient.getCompanies.query();
+export default function Home(props: PageProps<{ companies: CompanySelect[]} >) {
+  const { companies } = props.data;
 
   return (
     <div class="min-h-screen p-2 sm:p-4 mx-auto max-w-screen flex flex-col">
