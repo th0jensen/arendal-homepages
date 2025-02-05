0739bc7a0944757946ad4e47e2dbc363a81358c0
Thomas Jensen <thomas.jensen_@outlook.com>
2025-02-05 08:16:20 +0100

    css opt

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,6 +6,7 @@ export default function App({ Component }: PageProps) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>arendal.thojensen.com</title>
+        <link rel="preload" href="/styles.css" as="style" />
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
